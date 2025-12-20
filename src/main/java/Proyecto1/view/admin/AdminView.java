c7c9dbf1591b38d9a4fe6b325dcee8a7a7f2f3c7
Ariana Villota <aribeth1410@gmail.com>
2025-12-20 12:25:00 -0500

    Interfaz del Login

---
+++ src/main/java/Proyecto1/view/admin/AdminView.java
@@ -0,0 +1,17 @@
+package Proyecto1.view.admin;
+
+import javax.swing.*;
+
+public class AdminView extends JFrame {
+
+    private JPanel rootPanel;
+    private JLabel jbltitulo;
+    private JButton jbtgestorconductor;
+    private JButton jbjemitirlicencia;
+    private JButton jbjconsultar;
+    private JButton jbjpruebas;
+    private JButton jbjvalidar;
+    private JButton jbjcerrar;
+
+}
+

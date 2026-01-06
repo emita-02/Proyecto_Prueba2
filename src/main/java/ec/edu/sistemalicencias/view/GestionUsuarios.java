d93b923b72b0a578af2df0c5bf22c7e0ea4fb002
Aribeth105 <aribeth1410@gmail.com>
2026-01-05 20:58:35 -0500

    Interfaz de Gestion de Usuarios en Administrador

---
+++ src/main/java/ec/edu/sistemalicencias/view/GestionUsuarios.java
@@ -0,0 +1,30 @@
+package ec.edu.sistemalicencias.view;
+
+import javax.swing.*;
+
+public class GestionUsuarios {
+    private JPanel panelFormulario;
+    private JTextField txtCedula;
+    private JTextField txtContraseña;
+    private JTable table1;
+    private JButton btnRefrescar;
+    private JTextField txtCEDULA;
+    private JCheckBox cbAdmin;
+    private JCheckBox cbAnalista;
+    private JCheckBox cbActivo;
+    private JCheckBox cbDesactivado;
+    private JButton btnCrear;
+    private JButton btnActualizar;
+    private JButton btnLimpiar;
+    private JLabel lblTitulo;
+    private JLabel lblNombre;
+    private JLabel lblUserName;
+    private JLabel lblCedula;
+    private JLabel lblContraseña;
+    private JTextField txtNombre;
+    private JTextField txtUserName;
+    private JLabel lblCEDULA;
+    private JLabel lblRol;
+    private JLabel lblEstado;
+    private JButton btnRegresar;
+}
